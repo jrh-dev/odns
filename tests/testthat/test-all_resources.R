f9261f35c4a86e435aafbc66e4dcdf7346caae6e
James <jrh-dev@protonmail.com>
2022-06-29 11:28:02 +0100

    updated all_resources

--- tests/testthat/test-all_resources.R
+++ tests/testthat/test-all_resources.R
@@ -1,58 +1,28 @@
 
 testthat::test_that('no errors and successful run', {
   
-  load(testthat::test_path("test_data", "pck.rda"))
-  
   load(testthat::test_path("test_data", "pck_res.rda"))
   
-  mockery::stub(all_resources, 'all_packages', 
-                function(...) pck)
-  
-  mockery::stub(all_resources, 'package_metadata', 
-                function(...) pck_res)
+  mockery::stub(all_resources, "httr::GET", function(...) pck_res)
   
   testthat::expect_equal(
     digest::digest(all_resources(
       package_contains = "standard-populations",
       resource_contains = "European"
     )),
-    "ba503151d1131d40a31ca93cc8f12423")
+    "b8635f356b6b792c5123eb594bbb737c")
 })
 
-testthat::test_that('error when resource_contains value not found', {
-  
-  load(testthat::test_path("test_data", "pck.rda"))
+testthat::test_that('WARNING when arguments leads to 0 row data.frame', {
   
   load(testthat::test_path("test_data", "pck_res.rda"))
   
-  mockery::stub(all_resources, 'all_packages', 
-                function(...) pck)
+  mockery::stub(all_resources, "httr::GET", function(...) pck_res)
   
-  mockery::stub(all_resources, 'package_metadata', 
-                function(...) pck_res)
-  
-  testthat::expect_error(
-    all_resources(
-      package_contains = "standard-populations",
-      resource_contains = "a_resource_that_doesnt_exist"
-    ))
-})
-
-testthat::test_that('error when package_contains value not found', {
-  
-  load(testthat::test_path("test_data", "pck.rda"))
-  
-  load(testthat::test_path("test_data", "pck_res.rda"))
-  
-  mockery::stub(all_resources, 'all_packages', 
-                function(...) pck)
-  
-  mockery::stub(all_resources, 'package_metadata', 
-                function(...) pck_res)
-  
-  testthat::expect_error(
+  testthat::expect_warning(
     all_resources(
       package_contains = "a_package_that_doesnt_exist",
       resource_contains = "a_resource_that_doesnt_exist"
     ))
 })
+
